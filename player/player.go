3e48d172cf394ffd7f55ee03ed09d67202fe2737
jonahfried <35707907+jonahfried@users.noreply.github.com>
2018-04-24 18:47:20 -0400

    Moved Agent & Camera functions to a new Package

---
+++ player/player.go
@@ -0,0 +1,164 @@
+package player
+
+import (
+	"math"
+	"shadowRoom/boundry"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+// Agent keeps a posn, vel, acc, and a *pixel.IMDraw.
+// Used to keep all information for the movable image together.
+type Agent struct {
+	Posn, Vel, Acc pixel.Vec
+
+	Shade bool
+	Fill  bool
+
+	Img *imdraw.IMDraw
+}
+
+// MakeAgent creates a new agent starting at a given (x, y) coordinate
+func MakeAgent(x, y float64) (cir Agent) {
+	cir.Posn = pixel.V(x, y)
+	cir.Vel = pixel.ZV
+	cir.Acc = pixel.ZV
+
+	cir.Shade = true
+	cir.Fill = true
+
+	cir.Img = imdraw.New(nil)
+	cir.Img.Color = colornames.Purple
+	cir.Img.Push(pixel.V(500, 350))
+	cir.Img.Circle(20, 0)
+	return cir
+}
+
+// Vector limitation. Takes in a pixel.Vec and a float64.
+// If the magnitude of the given vector is greater than the limit,
+// Then the magnitude is scaled down to the limit.
+func limitVecMag(v pixel.Vec, lim float64) pixel.Vec {
+	if v.Len() != 0 && v.Len() > lim {
+		v = v.Scaled(lim / v.Len())
+	}
+	return v
+}
+
+// PressHandler Handles key presses.
+// Agent method taking in a window from which to accept inputs.
+func (cir *Agent) PressHandler(win *pixelgl.Window) {
+	cir.Acc = pixel.ZV
+	if win.Pressed(pixelgl.KeyA) {
+		cir.Acc = cir.Acc.Sub(pixel.V(5, 0))
+	}
+	if win.Pressed(pixelgl.KeyD) {
+		cir.Acc = cir.Acc.Add(pixel.V(5, 0))
+	}
+	if win.Pressed(pixelgl.KeyS) {
+		cir.Acc = cir.Acc.Sub(pixel.V(0, 5))
+	}
+	if win.Pressed(pixelgl.KeyW) {
+		cir.Acc = cir.Acc.Add(pixel.V(0, 5))
+	}
+	if win.JustPressed(pixelgl.KeyJ) {
+		cir.Posn.X--
+	}
+	if win.JustPressed(pixelgl.KeyL) {
+		cir.Posn.X++
+	}
+	if win.JustPressed(pixelgl.KeyK) {
+		cir.Posn.Y--
+	}
+	if win.JustPressed(pixelgl.KeyI) {
+		cir.Posn.Y++
+	}
+	if win.JustPressed(pixelgl.KeySpace) {
+		cir.Shade = !cir.Shade
+	}
+	if win.JustPressed(pixelgl.KeyF) {
+		cir.Fill = !cir.Fill
+	}
+}
+
+// ReleaseHandler Handles key releases.
+// Agent method taking in a window from which to accept inputs.
+func (cir *Agent) ReleaseHandler(win *pixelgl.Window) {
+	if win.JustReleased(pixelgl.KeyA) {
+		cir.Acc = cir.Acc.Add(pixel.V(5, 0))
+	}
+	if win.JustReleased(pixelgl.KeyD) {
+		cir.Acc = cir.Acc.Sub(pixel.V(5, 0))
+	}
+	if win.JustReleased(pixelgl.KeyS) {
+		cir.Acc = cir.Acc.Add(pixel.V(0, 5))
+	}
+	if win.JustReleased(pixelgl.KeyW) {
+		cir.Acc = cir.Acc.Sub(pixel.V(0, 5))
+	}
+}
+
+// Disp Handles display of an agent. Clears, pushes, adds shape, and draws.
+func (cir *Agent) Disp(win *pixelgl.Window) {
+	cir.Img.Clear()
+	cir.Img.Push(cir.Posn)
+	cir.Img.Circle(20, 0)
+	cir.Img.Draw(win)
+}
+
+// Update is an agent method. Runs all necessary per-frame proccedures on agent.
+// Takes in a pixelgl.Window from which to accept inputs.
+func (cir *Agent) Update(win *pixelgl.Window, room boundry.Place) {
+	cir.PressHandler(win)
+	cir.ReleaseHandler(win)
+
+	cir.Posn = cir.Posn.Add(cir.Vel)
+	cir.Vel = limitVecMag(cir.Vel.Add(limitVecMag(cir.Acc, 1.5)), 10).Scaled(.88)
+	if vecDist(cir.Vel, pixel.ZV) < .2 {
+		cir.Vel = pixel.ZV
+	}
+
+	cir.Posn.X = math.Max(cir.Posn.X-20, room.Rect.Min.X) + 20
+	cir.Posn.X = math.Min(cir.Posn.X+20, room.Rect.Max.X) - 20
+
+	cir.Posn.Y = math.Max(cir.Posn.Y-20, room.Rect.Min.Y) + 20
+	cir.Posn.Y = math.Min(cir.Posn.Y+20, room.Rect.Max.Y) - 20
+}
+
+func vecDist(v1, v2 pixel.Vec) float64 {
+	return math.Sqrt(math.Pow(v1.X-v2.X, 2) + math.Pow(v1.Y-v2.Y, 2))
+}
+
+// Camera is a struct storing a Matrix to set the window
+type Camera struct {
+	Posn, vel          pixel.Vec
+	maxForce, maxSpeed float64
+	Matrix             pixel.Matrix
+}
+
+// MakeCamera takes in a starting position and window and returns a Camera
+func MakeCamera(posn pixel.Vec, win *pixelgl.Window) (cam Camera) {
+	cam.Posn = posn
+	cam.vel = pixel.ZV
+	cam.maxSpeed = 10
+
+	cam.Matrix = pixel.IM.Moved(win.Bounds().Center().Sub(posn))
+
+	return cam
+}
+
+// Attract updates a Camera's velocity and position to follow a given point
+func (cam *Camera) Attract(target pixel.Vec) {
+	acc := limitVecMag(target.Sub(cam.Posn), vecDist(cam.Posn, target)/10)
+	// scale := cam.maxForce / 8
+	// acc = acc.Scaled(scale)
+	acc = acc.Sub(cam.vel)
+
+	cam.vel = cam.vel.Add(acc)
+	cam.vel = limitVecMag(cam.vel, cam.maxSpeed)
+
+	cam.Posn = cam.Posn.Add(cam.vel)
+
+}
--- shadowRoom.go
+++ shadowRoom.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"shadowRoom/boundry"
 	"shadowRoom/creature"
+	"shadowRoom/player"
 	"sort"
 	"time"
 
@@ -16,160 +17,8 @@ import (
 	"github.com/faiface/pixel/pixelgl"
 )
 
-// Type agent. Keeps a posn, vel, acc, and a *pixel.IMDraw.
-// Used to keep all information for the movable image together.
-type agent struct {
-	posn pixel.Vec
-	vel  pixel.Vec
-	acc  pixel.Vec
-
-	shade bool
-	fill  bool
-
-	img *imdraw.IMDraw
-}
-
-// Creates a new agent starting at a given (x, y) coordinate
-func makeAgent(x, y float64) (cir agent) {
-	cir.posn = pixel.V(x, y)
-	cir.vel = pixel.ZV
-	cir.acc = pixel.ZV
-
-	cir.shade = true
-	cir.fill = true
-
-	cir.img = imdraw.New(nil)
-	cir.img.Color = colornames.Purple
-	cir.img.Push(pixel.V(500, 350))
-	cir.img.Circle(20, 0)
-	return cir
-}
-
-// Vector limitation. Takes in a pixel.Vec and a float64.
-// If the magnitude of the given vector is greater than the limit,
-// Then the magnitude is scaled down to the limit.
-func limitVecMag(v pixel.Vec, lim float64) pixel.Vec {
-	if v.Len() != 0 && v.Len() > lim {
-		v = v.Scaled(lim / v.Len())
-	}
-	return v
-}
-
-// Handles key presses. Agent method taking in a window from which to accept inputs.
-func (cir *agent) pressHandler(win *pixelgl.Window) {
-	cir.acc = pixel.ZV
-	if win.Pressed(pixelgl.KeyA) {
-		cir.acc = cir.acc.Sub(pixel.V(5, 0))
-	}
-	if win.Pressed(pixelgl.KeyD) {
-		cir.acc = cir.acc.Add(pixel.V(5, 0))
-	}
-	if win.Pressed(pixelgl.KeyS) {
-		cir.acc = cir.acc.Sub(pixel.V(0, 5))
-	}
-	if win.Pressed(pixelgl.KeyW) {
-		cir.acc = cir.acc.Add(pixel.V(0, 5))
-	}
-	if win.JustPressed(pixelgl.KeyJ) {
-		cir.posn.X--
-	}
-	if win.JustPressed(pixelgl.KeyL) {
-		cir.posn.X++
-	}
-	if win.JustPressed(pixelgl.KeyK) {
-		cir.posn.Y--
-	}
-	if win.JustPressed(pixelgl.KeyI) {
-		cir.posn.Y++
-	}
-	if win.JustPressed(pixelgl.KeySpace) {
-		cir.shade = !cir.shade
-	}
-	if win.JustPressed(pixelgl.KeyF) {
-		cir.fill = !cir.fill
-	}
-}
-
-// Handles key releases. Agent method taking in a window from which to accept inputs.
-func (cir *agent) releaseHandler(win *pixelgl.Window) {
-	if win.JustReleased(pixelgl.KeyA) {
-		cir.acc = cir.acc.Add(pixel.V(5, 0))
-	}
-	if win.JustReleased(pixelgl.KeyD) {
-		cir.acc = cir.acc.Sub(pixel.V(5, 0))
-	}
-	if win.JustReleased(pixelgl.KeyS) {
-		cir.acc = cir.acc.Add(pixel.V(0, 5))
-	}
-	if win.JustReleased(pixelgl.KeyW) {
-		cir.acc = cir.acc.Sub(pixel.V(0, 5))
-	}
-}
-
-// Handles display of an agent. Clears, pushes, adds shape, and draws.
-func (cir *agent) disp(win *pixelgl.Window) {
-	cir.img.Clear()
-	cir.img.Push(cir.posn)
-	cir.img.Circle(20, 0)
-	cir.img.Draw(win)
-}
-
-// agent method. Runs all necessary per-frame proccedures on agent.
-// Takes in a pixelgl.Window from which to accept inputs.
-func (cir *agent) update(win *pixelgl.Window, room boundry.Place) {
-	cir.pressHandler(win)
-	cir.releaseHandler(win)
-
-	cir.posn = cir.posn.Add(cir.vel)
-	cir.vel = limitVecMag(cir.vel.Add(limitVecMag(cir.acc, 1.5)), 10).Scaled(.88)
-	if vecDist(cir.vel, pixel.ZV) < .2 {
-		cir.vel = pixel.ZV
-	}
-
-	// if !r.rect.Contains(cir.posn) {
-	// if cir.posn.X < r.rect.Center().X-100 { //-(r.rect.W()/2) {
-	// 	cir.posn.X = r.rect.Center().X - 100
-	// }
-	// cir.posn.X = math.Max(cir.posn.X, r.rect.Center().X-r.rect.W())
-	cir.posn.X = math.Max(cir.posn.X-20, room.Rect.Min.X) + 20
-	cir.posn.X = math.Min(cir.posn.X+20, room.Rect.Max.X) - 20
-
-	cir.posn.Y = math.Max(cir.posn.Y-20, room.Rect.Min.Y) + 20
-	cir.posn.Y = math.Min(cir.posn.Y+20, room.Rect.Max.Y) - 20
-	// }
-}
-
 func vecDist(v1, v2 pixel.Vec) float64 {
 	return math.Sqrt(math.Pow(v1.X-v2.X, 2) + math.Pow(v1.Y-v2.Y, 2))
-}
-
-type camera struct {
-	posn, vel          pixel.Vec
-	maxForce, maxSpeed float64
-	matrix             pixel.Matrix
-}
-
-func makeCamera(posn pixel.Vec, win *pixelgl.Window) (cam camera) {
-	cam.posn = posn
-	cam.vel = pixel.ZV
-	cam.maxSpeed = 10
-
-	cam.matrix = pixel.IM.Moved(win.Bounds().Center().Sub(posn))
-
-	return cam
-}
-
-func (cam *camera) attract(target pixel.Vec) {
-	acc := limitVecMag(target.Sub(cam.posn), vecDist(cam.posn, target)/10)
-	// scale := cam.maxForce / 8
-	// acc = acc.Scaled(scale)
-	acc = acc.Sub(cam.vel)
-
-	cam.vel = cam.vel.Add(acc)
-	cam.vel = limitVecMag(cam.vel, cam.maxSpeed)
-
-	cam.posn = cam.posn.Add(cam.vel)
-
 }
 
 // Acting main function
@@ -188,10 +37,10 @@ func run() {
 	// rooms := make(map[pixel.Vec]struct{ bottemLeft, topRight float64 })
 
 	// setting up the Player
-	var cir = makeAgent(0, 0)
+	var cir = player.MakeAgent(0, 0)
 
 	// setting up the camera
-	cam := makeCamera(cir.posn, win)
+	cam := player.MakeCamera(cir.Posn, win)
 
 	blockList := make([]boundry.Obsticle, 0)
 	for ind := 0; ind < 1; ind++ {
@@ -215,12 +64,12 @@ func run() {
 	for !win.Closed() {
 		win.Clear(colornames.Whitesmoke)
 
-		cir.update(win, room)
+		cir.Update(win, room)
 
-		cam.attract(cir.posn)
-		cam.matrix = pixel.IM.Moved(win.Bounds().Center().Sub(cam.posn))
+		cam.Attract(cir.Posn)
+		cam.Matrix = pixel.IM.Moved(win.Bounds().Center().Sub(cam.Posn))
 
-		win.SetMatrix(cam.matrix)
+		win.SetMatrix(cam.Matrix)
 
 		room.Img.Draw(win)
 
@@ -232,7 +81,7 @@ func run() {
 		for _, block := range room.Blocks {
 			if !block.IsRoom {
 				for _, vertex := range block.Vertices {
-					theta := math.Atan2((vertex.Y - cir.posn.Y), (vertex.X - cir.posn.X))
+					theta := math.Atan2((vertex.Y - cir.Posn.Y), (vertex.X - cir.Posn.X))
 					anglesToCheck = append(anglesToCheck, theta)
 				}
 				length := len(anglesToCheck)
@@ -243,9 +92,9 @@ func run() {
 				}
 				shadedVertices := make([]pixel.Vec, 0)
 				for _, vertex := range room.Vertices {
-					theta := math.Atan2((vertex.Y - cir.posn.Y), (vertex.X - cir.posn.X))
-					landed := boundry.Obstruct(cir.posn, theta, room, block)
-					if math.Abs(vecDist(landed, cir.posn)-vecDist(vertex, cir.posn)) > 1 {
+					theta := math.Atan2((vertex.Y - cir.Posn.Y), (vertex.X - cir.Posn.X))
+					landed := boundry.Obstruct(cir.Posn, theta, room, block)
+					if math.Abs(vecDist(landed, cir.Posn)-vecDist(vertex, cir.Posn)) > 1 {
 						// point.Push(vertex)
 						shadedVertices = append(shadedVertices, vertex)
 						// if !cir.shade {
@@ -256,15 +105,15 @@ func run() {
 				shapePoints := make([]pixel.Vec, 0)
 				sort.Float64s(anglesToCheck)
 				for _, angle := range anglesToCheck {
-					vec := boundry.Obstruct(cir.posn, angle, room, block)
+					vec := boundry.Obstruct(cir.Posn, angle, room, block)
 					shapePoints = append(shapePoints, vec)
 					point.Push(vec)
-					if !cir.shade {
+					if !cir.Shade {
 						point.Circle(4, 0)
 					}
 				}
-				if cir.shade {
-					if cir.fill {
+				if cir.Shade {
+					if cir.Fill {
 						point.Polygon(0)
 					} else {
 						point.Polygon(1)
@@ -273,19 +122,19 @@ func run() {
 				for _, vertex := range shadedVertices {
 					for vecInd := 1; vecInd < len(shapePoints); vecInd++ {
 						point.Push(vertex)
-						if !cir.shade {
+						if !cir.Shade {
 							point.Circle(4, 0)
 						}
 						point.Push(shapePoints[vecInd])
-						if !cir.shade {
+						if !cir.Shade {
 							point.Circle(4, 0)
 						}
 						point.Push(shapePoints[vecInd-1])
-						if !cir.shade {
+						if !cir.Shade {
 							point.Circle(4, 0)
 						}
-						if cir.shade {
-							if cir.fill {
+						if cir.Shade {
+							if cir.Fill {
 								point.Polygon(0)
 							} else {
 								point.Polygon(1)
@@ -297,19 +146,19 @@ func run() {
 				if len(shadedVertices) > 1 {
 					for vertexInd := 1; vertexInd < len(shadedVertices); vertexInd++ {
 						point.Push(shadedVertices[vertexInd-1])
-						if !cir.shade {
+						if !cir.Shade {
 							point.Circle(4, 0)
 						}
 						point.Push(shadedVertices[vertexInd])
-						if !cir.shade {
+						if !cir.Shade {
 							point.Circle(4, 0)
 						}
 						point.Push(shapePoints[0])
-						if !cir.shade {
+						if !cir.Shade {
 							point.Circle(4, 0)
 						}
-						if cir.shade {
-							if cir.fill {
+						if cir.Shade {
+							if cir.Fill {
 								point.Polygon(0)
 							} else {
 								point.Polygon(1)
@@ -331,7 +180,7 @@ func run() {
 			}
 		}
 
-		cir.disp(win)
+		cir.Disp(win)
 		win.Update()
 		// time.Sleep(1 / 2 * time.Second)
 	}
