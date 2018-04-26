d98b2625af8500064755d97f5c59d78ff45c10d1
jonahfried <35707907+jonahfried@users.noreply.github.com>
2018-04-26 18:09:59 -0400

    priority queue work

--- priorityQueue/priorityQueue.go
+++ priorityQueue/priorityQueue.go
@@ -7,11 +7,18 @@ import "shadowRoom/boundry"
 type PriorityQueue []elem
 
 type elem struct {
-	val    boundry.Tile
+	val    *boundry.Tile
 	weight float64
 }
 
 // Push is a PriorityQueue method, adding a given value to the PriorityQueue with a given weight.
 func (prior *PriorityQueue) Push(tl *boundry.Tile, weight float64) {
+	var newElem elem
+	newElem.val, newElem.weight = tl, weight
+	elemInd := len(*prior)
+	*prior = append(*prior, newElem)
+	for elemInd > 0 {
+
+	}
 
 }
